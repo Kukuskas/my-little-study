3d63896e0cb31046d19f25a78b4ebb16a2024be0
Alexandr Kozlov <alexandr.kozlov@seznam.cz>
2020-02-22 14:21:44 +0100

    counter  jsx eddited

--- myfirstreact/src/Components/counter.jsx
+++ myfirstreact/src/Components/counter.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 0,
+    // imageUrl: "https://picsum.photos/200"
+  };
   render() {
-    return <h1>Hello</h1>;
+    return (
+      <div>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Přidat</button>
+      </div>
+    );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Prázdno" : count;
   }
 }
 
