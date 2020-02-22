2b73425ba0607e0b9d54a780b7bcb0cac830455f
Alexandr Kozlov <alexandr.kozlov@seznam.cz>
2020-02-22 15:58:56 +0100

    Increment and Delete

--- myfirstreact/src/Components/counter.jsx
+++ myfirstreact/src/Components/counter.jsx
@@ -2,20 +2,34 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.counter.value
     // imageUrl: "https://picsum.photos/200"
   };
+    incrEvent=() =>{
+    this.setState({value: this.state.value +1});
+  }
   render() {
     return (
       <div>
         {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Přidat</button>
+        <span className={this.zeroWarning()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm" onClick={()=> this.incrEvent({id:1})}>
+          Přidat
+        </button>
+        <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>
+          Delete
+        </button>
       </div>
     );
   }
+
+  zeroWarning() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Prázdno" : count;
   }
 }
