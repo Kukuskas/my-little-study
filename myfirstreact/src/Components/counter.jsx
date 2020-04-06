e40a622b80f73c315e48c866290707459d4fe1b8
Alexandr Kozlov <alexandr.kozlov@seznam.cz>
2020-04-06 14:30:17 +0200

    Changed margin and added decrement disabled

--- myfirstreact/src/Components/counter.jsx
+++ myfirstreact/src/Components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.counter.value
+    value: this.props.counter.value,
     // imageUrl: "https://picsum.photos/200"
   };
   //   incrEvent=() =>{
@@ -10,42 +10,47 @@ class Counter extends Component {
   // }
   render() {
     return (
-      <div>
-        <span>Položka č.{this.props.counter.id}</span>
-        {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span style={{ width: 70 }} className={this.zeroWarning()}>
-          {this.formatCount()}
+      <div className="row">
+        <span style={{ width: 95, marginTop: 8 }}>
+          Položka č.{this.props.counter.id}
         </span>
-        <button
-          className="btn btn-secondary btn-sm"
-          onClick={() => this.props.onIncrement(this.props.counter)}
-        >
-          +
-        </button>
-        <button
-          className="btn btn-secondary btn-sm m-2"
-          onClick={() => this.props.onDecrement(this.props.counter)}
-        >
-          -
-        </button>
-        <button
-          className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-        >
-          Delete
-        </button>
+
+        <div style={{ textAlign: "center", width: 42, marginTop: 8 }}>
+          <span className={this.zeroWarning()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            className="btn btn-secondary btn-sm "
+            onClick={() => this.props.onIncrement(this.props.counter)}
+          >
+            +
+          </button>
+          <button
+            className="btn btn-secondary btn-sm m-2"
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
+          >
+            -
+          </button>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
   }
 
   zeroWarning() {
-    let classes = "badge m-2 badge-";
+    let classes = "badge badge-";
     classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
   formatCount() {
     const { value } = this.props.counter;
-    return value === 0 ? "Prázdno" : value;
+    return value === 0 ? "None" : value;
   }
 }
 
